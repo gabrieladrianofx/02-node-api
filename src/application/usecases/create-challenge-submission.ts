685e9396b7167708e51d441f3e9ff59fd86eecf1
gabrieladrianofx <gabrieladrianofx@gmail.com>
2022-10-12 18:43:07 -0300

    ✨ base project

---
+++ src/application/usecases/create-challenge-submission.ts
@@ -0,0 +1,34 @@
+import { Submission } from "../../domain/entities/submission";
+import { ChallengeRepository } from "../repositories/ChallengeRepository";
+import { StudentsRepository } from "../repositories/StudentsRepository";
+
+type CreateChallengeSubmissionRequest = {
+    studentId: string;
+    challengeId: string;
+}
+
+export class CreateChallengeSubmission {
+    constructor(
+        private studentsRepository: StudentsRepository,
+        private challengesRepository: ChallengeRepository,
+    ) {} 
+
+    async execute({studentId, challengeId}: CreateChallengeSubmissionRequest) {
+        const student = await this.studentsRepository.findById(studentId)
+
+        if(!student)
+            throw new Error('Students does not exists.')
+
+        const challenge = await this.challengesRepository.findById(challengeId)
+            
+        if(!challenge)
+            throw new Error('Challenge does not exists.')
+
+        const submission = Submission.create({
+            studentId,
+            challengeId,
+        })
+
+        return submission
+    }
+}
